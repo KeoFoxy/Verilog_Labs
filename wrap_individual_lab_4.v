89cb2e859a46def2762d00fb5d162edcac596ca0
Alexander <45719053+KeoFoxy@users.noreply.github.com>
2022-05-06 19:11:16 +0300

    key fix

--- wrap_individual_lab_4.v
+++ wrap_individual_lab_4.v
@@ -11,7 +11,7 @@ input [9:0] HEX2,
 input [9:0] HEX3
 );
 
-I_love_anime_1 init_1 (CLOCK_50, !KEY[0], !KEY[1],SW[9:0], HEX0[6:0],HEX1[6:0],HEX2[6:0],HEX3[6:0]);
-I_love_anime_2 init_2 (CLOCK_50, !KEY[0], !KEY[1],SW, LEDR[9:0]);
+I_love_anime_1 init_1 (CLOCK_50, KEY[0], KEY[1],SW[9:0], HEX0[6:0],HEX1[6:0],HEX2[6:0],HEX3[6:0]);
+I_love_anime_2 init_2 (CLOCK_50, KEY[0], KEY[1],SW, LEDR[9:0]);
 
 endmodule
