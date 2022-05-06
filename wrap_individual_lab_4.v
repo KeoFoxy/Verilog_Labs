9c3be415caa2f7053c493f5885a136fca55aada2
Alexander <45719053+KeoFoxy@users.noreply.github.com>
2022-05-06 19:12:14 +0300

    output fix

--- wrap_individual_lab_4.v
+++ wrap_individual_lab_4.v
@@ -4,11 +4,11 @@ input 	CLOCK_50,
 input [1:0] KEY,
 input [9:0] LEDR,
 input [9:0] SW,
-input [9:0] HEX1,
-input [9:0] HEX0,
+output [6:0] HEX1,
+output [6:0] HEX0,
 
-input [9:0] HEX2,
-input [9:0] HEX3
+output [6:0] HEX2,
+output [6:0] HEX3
 );
 
 I_love_anime_1 init_1 (CLOCK_50, KEY[0], KEY[1],SW[9:0], HEX0[6:0],HEX1[6:0],HEX2[6:0],HEX3[6:0]);
