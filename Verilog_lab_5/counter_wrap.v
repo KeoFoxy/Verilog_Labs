9aff5f9824a4846179c9320ae04254e512d16aba
Alexander <45719053+KeoFoxy@users.noreply.github.com>
2022-05-12 11:30:19 +0300

    Ledr fix

--- Verilog_lab_5/counter_wrap.v
+++ Verilog_lab_5/counter_wrap.v
@@ -13,5 +13,5 @@ wire [31:0] pulse_counter;
 wire [31:0] counter_status;
 
 counter init_1 (CLOCK_50, SW[0], SW[1], pulse_counter[31:0], counter_status[31:0]);
-screen init_2 (CLOCK_50, pulse_counter[31:0], counter_status[31:0], HEX0[6:0],HEX1[6:0], HEX2[6:0], HEX3[6:0]);
+screen init_2 (CLOCK_50, pulse_counter[31:0], counter_status[31:0], HEX0[6:0],HEX1[6:0], HEX2[6:0], HEX3[6:0], LEDR[9:0]);
 endmodule
